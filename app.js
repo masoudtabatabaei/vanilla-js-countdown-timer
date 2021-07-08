fad8d2d66f2d496eefd1074fbc5b97df741448ce
Masoud <masoudtabatabaei20@gmail.com>
2021-07-08 22:27:58 +0430

    implement start/pause/resume/stop/reset event handler

---
+++ app.js
@@ -0,0 +1,126 @@
+const timesArr = ["hours" , "minutes" , "seconds"];
+// input selectors
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+
+// default inputs value
+let def_hours , def_minutes , def_seconds;
+
+// btn selectors
+const btnStart = document.querySelector(".btn_start");
+const btnPause = document.querySelector(".btn_pause_resume");
+const btnStop = document.querySelector(".btn_stop");
+const btnReset = document.querySelector(".btn_reset");
+
+let interval;
+let isPause = false;
+
+// get current Total Time by seconds
+function getCurrentTotalTime() {
+    let hours_value = parseInt(hours.value);
+    let minutes_value = parseInt(minutes.value);
+    let seconds_value = parseInt(seconds.value);
+
+    let totalTimeSeconds = (hours_value * 60 * 60) + (minutes_value * 60) + seconds_value;
+
+    return totalTimeSeconds;
+}
+
+// start timer
+btnStart.addEventListener("click" , (event) => {
+    event.target.style.display = "none";
+    btnPause.style.display = "block";
+    btnPause.disabled = false;
+    btnStop.style.display = "block";
+    btnReset.style.display = "block";
+
+    def_hours = parseInt(hours.value);
+    def_minutes = parseInt(minutes.value);
+    def_seconds = parseInt(seconds.value);  
+
+    //console.log("start >> " + "hours : " + def_hours + " minutes : " + def_minutes + " seconds : " + def_seconds);
+
+    totalTimeSeconds = getCurrentTotalTime();
+    isPause = false;
+    
+    startTimer(totalTimeSeconds);
+});
+
+// set countdown timer
+function startTimer(totalTimeSeconds) {
+    interval = setInterval(()=>{
+        if (totalTimeSeconds > 0 && !isPause) {
+            totalTimeSeconds--;
+            updateTimeInputs(totalTimeSeconds);
+        } else if (totalTimeSeconds == 0 ) {
+            clearInterval(interval);
+            btnPause.disabled = true;
+        }
+    } , 1000);
+}
+
+// update inputs value
+function updateTimeInputs(totalTimeSeconds) {
+    let hours_updated = Math.floor(totalTimeSeconds / 3600) ;
+    let minutes_updated = Math.floor(Math.floor(totalTimeSeconds % 3600) / 60);
+    let seconds_updated = Math.floor(Math.floor(totalTimeSeconds % 3600) % 60) ;
+
+    //console.log(hours_updated , minutes_updated , seconds_updated);
+
+    timesArr.forEach(element => {
+        eval(element).disabled = true;
+        eval(element).value = eval(element+"_updated");
+    });
+}
+
+// pause event handler
+btnPause.addEventListener("click" , (event) => {
+    isPause = !isPause;
+    
+    if(isPause) {
+        event.target.textContent = "Resume";
+    } else {
+        event.target.textContent = "Pause";
+    }
+});
+
+
+// stop event handler
+btnStop.addEventListener("click" , () => {
+    btnStart.style.display = "block";
+    btnPause.style.display = "none";
+    btnPause.disabled = false;
+    btnStop.style.display = "none";
+    btnReset.style.display = "none";
+
+    isPause = true;
+
+    hours.value = def_hours;
+    minutes.value = def_minutes;
+    seconds.value = def_seconds;
+
+    //console.log("pause >> " + "hours : " + def_hours + " minutes : " + def_minutes + " seconds : " + def_seconds);
+
+    timesArr.forEach(element => {
+        eval(element).disabled = false;
+    });
+
+    clearInterval(interval);
+});
+
+
+// reset event handler
+btnReset.addEventListener("click" , () => {
+    clearInterval(interval);
+    btnPause.disabled = false;
+
+    isPause = false;
+    
+    hours.value = def_hours;
+    minutes.value = def_minutes;
+    seconds.value = def_seconds;
+
+    totalTimeSeconds = getCurrentTotalTime();
+    startTimer(totalTimeSeconds);
+});
